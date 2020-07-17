000b9bba8bde001c6939078611f742e15a098e61
Dustin Crawford <dustin.crawford.2@gmail.com>
2020-07-17 14:41:52 -0400

    add author templates

--- books_authors_app/urls.py
+++ books_authors_app/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('', views.index),
     path('add_book', views.addBook),
     path('book_details/<int:book_id>', views.bookDetails),
-    path('book_details/<int:book_id>/append_author', views.appendAuthor)
+    path('book_details/<int:book_id>/append_author', views.appendAuthor),
+    path('authors', views.authors),
+    path('add_author', views.addAuthor),
+    path('author_details/<int:author_id>', views.authorDetails),
+    path('author_details/<int:author_id>/append_book', views.appendBook),
 ]
--- books_authors_app/views.py
+++ books_authors_app/views.py
@@ -24,3 +24,26 @@ def appendAuthor(request, book_id):
     book = Book.objects.get(id=book_id)
     book.authors.add(appAuthor)
     return redirect("/book_details/" + str(book.id))
+
+def authors(request):
+    context = {
+        'authors': Author.objects.all()
+    }
+    return render(request, 'authors.html', context)
+
+def addAuthor(request):
+    newAuthor = Author.objects.create(first_name=request.POST['first_name'], last_name=request.POST['last_name'], notes=request.POST['notes'])
+    return redirect('/authors')
+
+def authorDetails(request, author_id):
+    context = {
+        'author': Author.objects.get(id=author_id),
+        'books': Book.objects.all()
+    }
+    return render(request, 'author_details.html', context)
+
+def appendBook(request, author_id):
+    appBook = Book.objects.get(id=request.POST['other_books'])
+    author = Author.objects.get(id=author_id)
+    author.books.add(appBook)
+    return redirect('/author_details/' + str(author_id))
